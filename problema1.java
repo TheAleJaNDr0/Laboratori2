5b3ca6182898127f16b3cf1e25706169986b5e97
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-12-03 19:12:35 +0000

    problem 1 done

---
+++ problema1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class problema1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introdueix una seqüència de nombres: ");
+        boolean trobat = false;
+        while (sc.hasNextInt()) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                break;
+            }
+            if (x % 2 == 0) {
+                trobat = true;
+            }
+        }
+        if (trobat == true) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
