8fb3532a56485fbee5bdfe42dd92a67bbc1e0c79
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-12-03 23:33:05 +0000

    problem 2 in process

---
+++ problema2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class problema2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) {
+            return;
+        }
+        int digit = sc.nextInt();
+    }
+}
