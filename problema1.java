1797f45b1c8e1848723e4813b5f519f27dc7f9da
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-12-09 23:26:12 +0000

    changes done

--- problema1.java
+++ problema1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class problema1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Aquest es el problema 1");
         System.out.println("Introdueix una seqüència de nombres: ");
         boolean trobat = false;
         while (sc.hasNextInt()) {
--- problema2.java
+++ problema2.java
@@ -3,9 +3,32 @@ import java.util.Scanner;
 public class problema2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Aquest es el problema 1");
+        System.out.println("Introdueix un numero x: ");
         if (!sc.hasNextInt()) {
             return;
         }
-        int digit = sc.nextInt();
+        int x = sc.nextInt();
+        System.out.println("Introdueix un numero y: ");
+        if (!sc.hasNextInt()) {
+            return;
+        }
+        int y = sc.nextInt();
+        boolean trobat = false;
+
+        while (y > 0) {
+            int digit = y % 10;
+            if (digit == x) {
+                trobat = true;
+                break;
+            }
+            y = y / 10;
+        }
+        if (trobat == true) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+        sc.close();
     }
 }
